e58f19912d2c1ce4def0cd3d3df023b6ecd373fc
Igor <igormedeiros407@gmail.com>
2024-07-24 20:23:12 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//algoritimo que solicita um número inteiro positivo ao usuario e no final
+//imprima os numeros inpares do 1 até o numero que o usuario digitou
+
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int digiteNumero = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um número inteiro positivo"));
+
+        for (int i = 1; i <= digiteNumero; i++) {
+            if (i % 2 == 1){
+                System.out.println("Impar: " + i);
+            }
+
         }
+
+
     }
 }
